bde9fa34bdaa4dfb42f1302c20b9a2aec03e9a3b
edupoch <edupoch@gmail.com>
2025-01-08 13:46:03 +0100

    Conexión con dominio custom

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://edupoch.github.io",
-  base: "astro-pages-cms-demo",
+  site: "http://astro-pages-cms.estudiocaravana.com",
 });
